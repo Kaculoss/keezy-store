7ea65a61de54a8a792994f42c34adcb43e108487
Kaculoss <benjerosani99@gmail.com>
2023-11-07 01:49:20 +0000

    updated homepage

--- app/(site)/(home)/page.tsx
+++ app/(site)/(home)/page.tsx
@@ -22,7 +22,6 @@ export default async function Home() {
 
   return (
     <>
-      Home Page
       <CarouselSlider trendingProducts={trendingProducts} />
       <div className="mt-6">
         <FeaturedProducts featuredProducts={featuredProducts} />
